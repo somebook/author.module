1d8b743bfa4cd58aaa514eba2e7dd565c6868ec6
Denis Alekseev <alekseevdy@gmail.com>
2012-10-03 14:52:36 +0400

    fix

--- app/controllers/author/posts_controller.rb
+++ app/controllers/author/posts_controller.rb
@@ -135,7 +135,7 @@ class PostsController < SpaceController
         job = Delayed::Job.find_by_id(@post.job_id)
         job.destroy if job
         if @post.publish_at && @post.publish_at > Time.now
-          @post.delay! unless @post.delayed?
+          @post.delay! unless @post.delayed? || @post.published?
           job = Delayed::Job.enqueue(PublishingJob.new(@current_shard, @post), 0, @post.publish_at) unless Rails.env.test?
           @post.update_attribute(:job_id, job.id)
         else
